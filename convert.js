0a418e66faf47d71cba4dafc051bc52bab94826a
Elias Grünzweil <elias.gruenzweil@gmail.com>
2018-10-24 12:49:19 +0200

    Finished Converter

--- convert.js
+++ convert.js
@@ -1,1 +1,10 @@
-console.log('Not implemented yet!');
+var convert = require('convert-units'); const a = process.argv; var b;
+if (a[4] == "to" && a.length == 6 && isNaN(a[2]) == false) {
+    try {
+        b = convert(a[2]).from(a[3]).to(a[5]); console.log(a[2] + " " + a[3] + " are " + b + " " + a[5]);
+    } catch (ReferenceError) {
+        console.log("Invalid parameters");
+    }
+} else {
+    console.log("Invalid parameters");
+}
